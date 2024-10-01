66f0d1651d769a991aa42763ba422127fa0bc50f
Jonas Knygberg Kristiansen <jok003@edu.zealand.dk>
2024-10-01 10:58:23 +0200

    Unittests and carclass

---
+++ TicketClassLibrary/Classes/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketClassLibrary.Classes
+{
+    public class Car
+    {
+        public string Licenseplate { get; set; }
+        public DateTime Date { get; set; }
+
+        public double Price()
+        {
+            return 240.0;
+        }
+
+        public string VehicleType()
+        {
+            return "Car";
+        }
+
+    }
+}
---
+++ TicketClassLibraryTests/Classes/CarTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicketClassLibrary.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketClassLibrary.Classes.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        [TestMethod()]
+        public void PriceTest()
+        {
+            //arrange
+            var car = new Car();
+
+            //act
+            var Price = car.Price();
+
+            //assert
+            Assert.AreEqual(240.0, Price);
+        }
+
+        [TestMethod()]
+        public void VehicleTypeTest()
+        {
+            //arrange
+            var car = new Car();
+
+            //act
+            var VehicleType = car.VehicleType();
+
+            //assert
+            Assert.AreEqual("Car", VehicleType);
+        }
+    }
+}
