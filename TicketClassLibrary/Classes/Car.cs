d653a0dedc5a17ae28b0523354d4f009c408f02a
Jonas Knygberg Kristiansen <jok003@edu.zealand.dk>
2024-10-01 11:00:26 +0200

    forgot Comment

--- TicketClassLibrary/Classes/Car.cs
+++ TicketClassLibrary/Classes/Car.cs
@@ -8,14 +8,23 @@ namespace TicketClassLibrary.Classes
 {
     public class Car
     {
+        //properties
         public string Licenseplate { get; set; }
         public DateTime Date { get; set; }
 
+        //methods
+        /// <summary>
+        /// Price will always return 240
+        /// </summary>
+        /// <returns>240</returns>
         public double Price()
         {
             return 240.0;
         }
-
+        /// <summary>
+        /// Can only return Car
+        /// </summary>
+        /// <returns>Car</returns>
         public string VehicleType()
         {
             return "Car";
